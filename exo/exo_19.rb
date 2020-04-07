34aba9b6f960770563f18fb1bc8424a7c31ccba7
Popikadir <62880018+Popikadir@users.noreply.github.com>
2020-04-07 16:14:29 +0200

    Add files via upload

--- exo/exo_19.rb
+++ exo/exo_19.rb
@@ -1,11 +1,11 @@
 email  =  []
 num = 1
-x = 1
+a = 1
 50.times do
     email << "julien.doe.#{num}@email.com"
     num +=1
 end
 25.times do
-    puts email[x]
-    x += 2
+    puts email[a]
+    a += 2
 end
